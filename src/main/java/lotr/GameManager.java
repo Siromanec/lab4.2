744c7bfbded8763a901eba888a3620b25f6f8775
Siromanec <serhii.ivanov@ucu.edu.ua>
2022-10-13 23:05:05 +0300

    add game manager

--- src/main/java/lotr/GameManager.java
+++ src/main/java/lotr/GameManager.java
@@ -8,4 +8,34 @@ public class GameManager {
      *               the characters are alive
      *             }
      *             */
-}
+    static boolean processFight(Character attacker, Character deffender){
+        if (attacker.getHp() > 0) {
+            attacker.kick(deffender);
+            System.out.println(attacker + " kicks " + deffender);
+            return true;
+        }else{
+            System.out.println(attacker + " has perished");
+            return false;
+        }
+
+    }
+    public static void fight(Character c1, Character c2){
+        boolean bothAreAlive = true;
+        while (bothAreAlive) {
+            bothAreAlive = processFight(c1, c2);
+            if (!bothAreAlive) break;
+            bothAreAlive = processFight(c2, c1);
+            if (!bothAreAlive) break;
+
+
+        }
+        }
+
+    public static void main(String[] args) {
+        Character c1 = CharacterFactory.createCharacter();
+        Character c2 = CharacterFactory.createCharacter();
+
+        fight(c1, c2);
+    }
+    }
+
